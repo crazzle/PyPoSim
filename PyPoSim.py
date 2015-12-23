687ff29a5de7b6a79d9859e5dcc23cd9ef693688
Mark Keinhörster <mark.keinhoerster@gmail.com>
2015-12-23 01:08:01 +0100

    added threaded scheduler, moved logic out of actor, removed sched, no blocking on actor anymore for 1 sec

--- PyPoSim.py
+++ PyPoSim.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from actor import SimplePlantActor
+import time, threading
 
 
 ref = SimplePlantActor.SimplePlantActor.start()
 app = Flask(__name__)
-
 
 @app.route('/')
 def get_power_for_plant():
@@ -17,6 +17,21 @@ def set_power_target_for_plant(point):
     ref.tell({'msg': 'dispatch', 'value': point})
     return str(point)
 
+
+def tell_tick():
+    ref.tell({'msg': 'tick'})
+
+
+def schedule_tick():
+    tell_tick()
+    time.sleep(1)
+    run_async_tick()
+
+
+def run_async_tick():
+    threading.Thread(target=schedule_tick).start()
+
+run_async_tick()
 
 if __name__ == '__main__':
     app.debug = True
--- actor/SimplePlantActor.py
+++ actor/SimplePlantActor.py
@@ -1,30 +1,18 @@
 from plant import SimplePlant
-import time, sched, pykka
-
+import pykka
 
 class SimplePlantActor(pykka.ThreadingActor):
 
     def __init__(self):
         super(SimplePlantActor, self).__init__()
         self.plant = SimplePlant.SimplePlant(100, 15, 5)
-        self.now = time.time()
-        self.scheduler = sched.scheduler(time.time, time.sleep)
-        self.schedule_tick()
 
     def on_receive(self, message):
-        if message["msg"] == "start":
-            self.actor_ref.tell({'msg': 'tick'})
-
-        elif message["msg"] == "tick":
+        if message["msg"] == "tick":
             self.plant = self.plant.evolve()
-            self.schedule_tick()
 
         elif message["msg"] == "dispatch":
             self.plant = self.plant.dispatch(int(message["value"]))
 
         elif message["msg"] == "stats":
             return self.plant.output
-
-    def schedule_tick(self):
-        self.scheduler.enter(1, 1, self.actor_ref.tell, ({'msg': 'tick'},))
-        self.scheduler.run()
