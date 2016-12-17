0d03f9c266e722bc01565f7d895ded8d76daa4a2
Mark Keinhörster <mark.keinhoerster@gmail.com>
2016-12-17 21:19:55 +0100

    improve user interface

--- plantstorage/PlantStorage.py
+++ plantstorage/PlantStorage.py
@@ -37,7 +37,7 @@ class PlantStorage:
     def get_all_plants(self):
         connection = sqlite3.connect(self.db)
         cursor = connection.cursor()
-        rows = cursor.execute("select * from plant")
+        rows = cursor.execute("select * from plant ORDER BY name ASC")
         connection.commit()
         entries = list()
         for row in rows:
